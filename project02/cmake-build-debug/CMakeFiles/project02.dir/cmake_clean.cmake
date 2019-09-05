0132a1c8a931e54179571402e91c468441607b00
norbit8 <norbit8@gmail.com>
2019-09-05 16:17:15 +0300

    oh shit here we go again (project02)

---
+++ project02/cmake-build-debug/CMakeFiles/project02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project02.dir/main.cpp.o"
+  "project02.pdb"
+  "project02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
